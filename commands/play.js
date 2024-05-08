7a082c686ff535c399f27c51bdca15f8132d6226
hoanganh2321 <131464214+hoanganh2321@users.noreply.github.com>
2024-05-08 18:44:23 +0700

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -81,8 +81,8 @@ const embeds = [
         .setColor('#4d9fd6')
         .setAuthor({
             name: 'Yêu cầu cập nhật!',
-            iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236794583732457473/7828-verify-ak.gif?ex=66394e37&is=6637fcb7&hm=923d3f3b300606a2ae4ceb7bae980fd533a4c5ee2cf73111569a892a595f1f69&', 
-            url: 'https://discord.gg/xQF9f9yUEM'
+            iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif?ex=663cadfd&is=663b5c7d&hm=f6b12171bc750db28036e82fe746a50622d138f99bc48a3cfb0390a5d32ca52c&', 
+            url: 'https://discord.gg/XBHZUwqAzK'
         })
         .setDescription('❗ **Yêu cầu của bạn đã được xử lý thành công.**\n❗** Vui lòng sử dụng các nút để kiểm soát hàng đợi**'),
 
@@ -91,8 +91,8 @@ const embeds = [
     .setColor('#ffea00')
     .setAuthor({
         name: 'Yêu cầu cập nhật!',
-        iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236802032938127470/4104-verify-yellow.gif?ex=66395527&is=663803a7&hm=71a7fba7f91897e52d9645b45d85d3da0ff97af2b63d10960004e68ff40d9c3b&', 
-        url: 'https://discord.gg/xQF9f9yUEM'
+        iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif?ex=663cadfd&is=663b5c7d&hm=f6b12171bc750db28036e82fe746a50622d138f99bc48a3cfb0390a5d32ca52c&', 
+        url: 'https://discord.gg/XBHZUwqAzK'
     })
     .setDescription('❗ **Yêu cầu của bạn đã được xử lý thành công.**\n❗** Vui lòng sử dụng các nút để kiểm soát hàng đợi**'),
 
@@ -101,8 +101,8 @@ const embeds = [
     .setColor('#FF0000')
     .setAuthor({
         name: 'Yêu cầu cập nhật!',
-        iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236802049190920202/4104-verify-red.gif?ex=6639552b&is=663803ab&hm=8dbc851fe56441b916a0044152dd517ed26434a11ee506518382f380c527c3bd&', 
-        url: 'https://discord.gg/xQF9f9yUEM'
+        iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230836684774576168/7762-verified-blue.gif?ex=663cadfd&is=663b5c7d&hm=f6b12171bc750db28036e82fe746a50622d138f99bc48a3cfb0390a5d32ca52c&', 
+        url: 'https://discord.gg/XBHZUwqAzK'
     })
     .setDescription('❗ **Yêu cầu của bạn đã được xử lý thành công.**\n❗** Vui lòng sử dụng các nút để kiểm soát hàng đợi**')
 ];
