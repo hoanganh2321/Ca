5af8c365a5973bc6daaf456e9a9c97c73bf4681e
hoanganh2321 <131464214+hoanganh2321@users.noreply.github.com>
2024-05-08 11:26:50 +0700

    Update player.js

--- player.js
+++ player.js
@@ -63,7 +63,7 @@ function initializePlayer(client) {
             iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1236664581364125787/music-play.gif?ex=6638d524&is=663783a4&hm=5179f7d8fcd18edc1f7d0291bea486b1f9ce69f19df8a96303b75505e18baa3a&', 
             url: 'https://discord.gg/xQF9f9yUEM'
         })
-        .setDescription(`🎹 **Tên Bài Hát:** [${track.info.title}](${track.info.uri})\n😃 **Nghệ Sĩ:** ${track.info.author}\n🎵 **Nền Tảng :** YouTube, Spotify, SoundCloud`)
+        .setDescription(`:music:  **Tên Bài Hát:** [${track.info.title}](${track.info.uri})\n:3177magicwandcom1:  **Nghệ Sĩ:** ${track.info.author}\n:media43:  **Nền Tảng :** :Youtube: , :Spotify: `)
 
         .setImage(`https://cdn.discordapp.com/attachments/1004341381784944703/1165201249331855380/RainbowLine.gif?ex=663939fa&is=6637e87a&hm=e02431de164b901e07b55d8f8898ca5b1b2832ad11985cecc3aa229a7598d610&`)
         .setThumbnail(track.info.thumbnail)
